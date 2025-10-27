c92bdb5131ccd640de9383da23694c204d767ae8
Juaneg0 <juanegomezgomez@gmail.com>
2025-10-26 21:38:57 -0500

    Funcional_LED_Button

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <Arduino.h>
 
-
+#define LED_PIN 13
+#define SWITCH_PIN 15
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3)
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(SWITCH_PIN) == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("LED ON");
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("LED OFF");
+  }
 }
 
